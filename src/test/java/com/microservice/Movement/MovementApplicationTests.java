5a24f797f10ebde27a53f5d1e6dd3dc9cd2eeaed
davisvitate <davisvitate@gmail.com>
2019-12-27 13:40:12 -0500

    first movement

---
+++ src/test/java/com/microservice/Movement/MovementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.Movement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
