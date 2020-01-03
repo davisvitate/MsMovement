4d0aa374c0686d8096d03573a74e8460bbf763b3
davisvitate <davisvitate@gmail.com>
2020-01-03 15:06:17 -0500

    funciona movimiento

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8
+VOLUME /tmp
+EXPOSE 8802
+ADD ./target/Movement-0.0.1-SNAPSHOT.jar movement-server.jar
+ENTRYPOINT ["java","-jar","/Movement-server.jar"]
--- src/main/java/com/microservice/Movement/controller/MovementController.java
+++ src/main/java/com/microservice/Movement/controller/MovementController.java
@@ -4,11 +4,11 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.support.WebExchangeBindException;
+
 
 
 
@@ -32,7 +32,7 @@ import reactor.core.publisher.Mono;
 
 
 @RestController
-@RequestMapping("/api/movement")
+@RequestMapping("/movement")
 public class MovementController {
 	
 	
@@ -40,7 +40,7 @@ public class MovementController {
 	@Autowired
 	private MovementServices service;
 	
-	@GetMapping("/movement")
+	@GetMapping
 	public Mono<ResponseEntity<Flux<Movement>>> lista(){
 		return Mono.just(
 				ResponseEntity.ok()
@@ -49,39 +49,43 @@ public class MovementController {
 				);
 	}
 	
-	
 	@PostMapping("/createM")
-	public Mono<ResponseEntity<Map<String, Object>>> createMove(@Valid @RequestBody Mono<Movement> monoMovement){
-		
-		Map<String, Object> respuesta = new HashMap<String, Object>();
-		
-		return monoMovement.flatMap(move->{
-			return service.saveMove(move).map(c->{
-				respuesta.put("movement", c);
-				respuesta.put("mensaje", "Movimiento creado con exito");
-				return ResponseEntity
-						.created(URI.create("/api/movement/createM".concat(c.getId())))
-						.contentType(MediaType.APPLICATION_JSON_UTF8)
-						.body(respuesta);
-						
-					
-			});
-		}).onErrorResume(t -> {
-			return Mono.just(t).cast(WebExchangeBindException.class)
-					.flatMap(e -> Mono.just(e.getFieldErrors()))
-					.flatMapMany(Flux::fromIterable)
-					.map(fieldError -> "El campo "+fieldError.getField() + " " + fieldError.getDefaultMessage())
-					.collectList()
-					.flatMap(list -> {
-						respuesta.put("errors", list);
-						respuesta.put("status", HttpStatus.BAD_REQUEST.value());
-						return Mono.just(ResponseEntity.badRequest().body(respuesta));
-					});
-							
-		});
-		
-
+	public Mono<Movement> createMove(@RequestBody Movement monoMovement){
+		return service.saveMove(monoMovement);
 	}
+	
+//	@PostMapping("/createM")
+//	public Mono<ResponseEntity<Map<String, Object>>> createMove(@Valid @RequestBody Mono<Movement> monoMovement){
+//		
+//		Map<String, Object> respuesta = new HashMap<String, Object>();
+//		
+//		return monoMovement.flatMap(move->{
+//			return service.saveMove(move).map(c->{
+//				respuesta.put("movement", c);
+//				respuesta.put("mensaje", "Movimiento creado con exito");
+//				return ResponseEntity
+//						.created(URI.create("/api/movement/createM".concat(c.getId())))
+//						.contentType(MediaType.APPLICATION_JSON_UTF8)
+//						.body(respuesta);
+//						
+//					
+//			});
+//		}).onErrorResume(t -> {
+//			return Mono.just(t).cast(WebExchangeBindException.class)
+//					.flatMap(e -> Mono.just(e.getFieldErrors()))
+//					.flatMapMany(Flux::fromIterable)
+//					.map(fieldError -> "El campo "+fieldError.getField() + " " + fieldError.getDefaultMessage())
+//					.collectList()
+//					.flatMap(list -> {
+//						respuesta.put("errors", list);
+//						respuesta.put("status", HttpStatus.BAD_REQUEST.value());
+//						return Mono.just(ResponseEntity.badRequest().body(respuesta));
+//					});
+//							
+//		});
+//		
+//
+//	}
 	
 	
 	
